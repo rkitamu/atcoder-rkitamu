0c12c3ce88a12252ccc4b88d8c20a8128c125a82
rkitamu <rkitamun@gmail.com>
2025-08-13 11:17:57 +0000

    practice

--- problems/practice/b/Program.cs
+++ problems/practice/b/Program.cs
@@ -5,5 +5,15 @@ class Program
 {
     static void Main()
     {
+        var abc = Console.ReadLine();
+        var cnt = 0;
+        for (int i = 0; i < abc.Length; i++)
+        {
+            if (abc[i] == '1')
+            {
+                cnt++;
+            }
+        }
+        Console.WriteLine(cnt);
     }
 }
